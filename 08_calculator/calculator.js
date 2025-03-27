181ea3beaec0bc01c07bfc648966d61111d2eaa2
liam-621 <195730031+liam-621@users.noreply.github.com>
2025-03-27 12:33:39 +1100

    Create working factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,14 @@ const power = function(a, b) {
 	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else {
+    return (num * factorial(num -1));
+  }
 };
 
 // Do not edit below this line
