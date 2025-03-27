5d0d51e9cbda27efccec9d73844d4d092ae669fe
liam-621 <195730031+liam-621@users.noreply.github.com>
2025-03-27 11:43:30 +1100

    Finish exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (numOne > numTwo) {
+        let temp = numOne;
+        numOne = numTwo;
+        numTwo = temp;
+    } 
+    if (numOne < 0 || numTwo < 0 || !Number.isInteger(numOne) || !Number.isInteger(numTwo)) return 'ERROR';
+    let finalSum = 0;
+    for (i = numOne; i <= numTwo; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let converted = ((x - 32) * 0.555555556);
+  return Math.round(converted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  let converted = (y * 1.8 + 32);
+  return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
