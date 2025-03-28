f0992fa6b40c2443636c27c4d8727d813fcf82d3
liam-621 <195730031+liam-621@users.noreply.github.com>
2025-03-28 16:56:24 +1100

    Create working fibbonacci return function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number == 0) {
+        return 0;
+    } else if (number < 0) {
+        return 'OOPS';
+    } else { 
+        let n1 = 1;
+        let n2 = 1;
+        let nextTerm;
+        let fibonacciArray = [];
 
+        for (let i = 1; i <= number; i++) {
+            fibonacciArray.push(n1);
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
+
+        return fibonacciArray[number - 1];
+    }
 };
 
 // Do not edit below this line
